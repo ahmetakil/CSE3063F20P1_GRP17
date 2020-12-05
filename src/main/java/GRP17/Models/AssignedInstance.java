5884eef6de939ea5e0fbf839c5025fdf78bdd99f
abdullahcansiz <74601598+abdullahcansiz@users.noreply.github.com>
2020-12-05 19:28:47 +0300

    Add files via upload

---
+++ src/main/java/GRP17/Models/AssignedInstance.java
@@ -0,0 +1,51 @@
+package GRP17.Models;
+import GRP17.UserModels.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class AssignedInstance {
+
+    private Instance instance;
+    private List<Label> labels;
+    private User user;
+    private Date date;
+
+    public AssignedInstance(User user, Instance instance, List<Label> labels,Date date) {
+        this.instance = instance;
+        this.labels = labels;
+        this.date = date;
+        this.user = user;
+    }
+
+    public Instance getInstance() {
+        return instance;
+    }
+
+    public JsonObject toJson(){
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("instance id",instance.getId());
+
+
+        JsonArray labelIdsJson = new JsonArray();
+
+        for(Label label : this.labels){
+            labelIdsJson.add(label.getId());
+        }
+
+        jsonObject.add("class labels id",labelIdsJson);
+        jsonObject.addProperty("user id",this.user.getId());
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy, hh:mm:ss");
+        jsonObject.addProperty("datetime",dateFormat.format(date) );
+        return jsonObject;
+    }
+
+    public String toString(){
+        return  "AssignedInstance <" +  instance.toString() + Arrays.toString(this.labels.toArray()) + ">";
+    }
+}
