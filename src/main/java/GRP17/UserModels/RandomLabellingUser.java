c312a6dd28ed2ba9ac65a9f3cc0996802bb84a45
Ahmet Akıl <ahmet.deathoftime@gmail.com>
2020-12-05 19:07:52 +0300

    Implemented the abstract User model and its subclass RandomLabellingUser

---
+++ src/main/java/GRP17/UserModels/RandomLabellingUser.java
@@ -0,0 +1,29 @@
+package GRP17.UserModels;
+
+
+import GRP17.Models.AssignedInstance;
+import GRP17.Models.Instance;
+import GRP17.Models.Label;
+
+import java.util.*;
+
+public class RandomLabellingUser extends User {
+
+    public RandomLabellingUser(int id, String name, String type) {
+        super(id, name, type);
+    }
+
+    @Override
+    public AssignedInstance assignLabel(Instance instance, List<Label> labels, int maxNumberOfLabelsPerInstance) {
+
+        Collections.shuffle(labels);
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(maxNumberOfLabelsPerInstance) + 1;
+
+        List<Label> subset = labels.subList(0, randomNumber);
+        AssignedInstance assignedInstance = new AssignedInstance(this, instance, subset, new Date());
+
+        return assignedInstance;
+    }
+}
---
+++ src/main/java/GRP17/UserModels/User.java
@@ -0,0 +1,38 @@
+package GRP17.UserModels;
+
+import GRP17.Models.AssignedInstance;
+import GRP17.Models.Instance;
+import GRP17.Models.Label;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public abstract class User {
+    @SerializedName("user id")
+    private int id;
+    @SerializedName("user name")
+    private String name;
+    @SerializedName("user type")
+    private String type;
+    private List<AssignedInstance> labellingRequests;
+
+    public User(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<AssignedInstance> getLabellingRequests() {
+        return labellingRequests;
+    }
+
+    public abstract AssignedInstance assignLabel(Instance instance, List<Label> labels, int maxNumberOfLabelsPerInstance);
+}
