6df778fc9345f2d095b7d1f6802ca87f0a0ff7b1
Zeynep Alıcı <aliciizeynep@gmail.com>
2020-12-05 19:32:47 +0300

    Create Main Class

---
+++ src/main/java/GRP17/Main.java
@@ -0,0 +1,49 @@
+package GRP17;
+
+import GRP17.IOController.ConfigParser;
+import GRP17.IOController.InputParser;
+import GRP17.IOController.OutputWriter;
+import GRP17.IOController.Parser;
+import GRP17.Models.*;
+import GRP17.UserModels.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+
+        Parser inputParser = new InputParser("assets/input.json");
+        Parser configParser = new ConfigParser("assets/config.json");
+
+        ConfigSet configSet = ((ConfigParser) configParser).parse();
+        DataSet dataSet = ((InputParser) inputParser).parse();
+
+        List<User> allUsers = configSet.getUsers();
+
+        List<Instance> allInstances = dataSet.getInstances();
+        List<Label> allLabels = dataSet.getLabels();
+
+
+        List<AssignedInstance> allAssignedInstance = new ArrayList<AssignedInstance>();
+
+        List<Instance> getInstancesToBeLabelled = allInstances;
+
+        for (Instance instance : getInstancesToBeLabelled) {
+
+            for (User user : allUsers) {
+
+                AssignedInstance assignedInstance = user.assignLabel(instance, allLabels, dataSet.getMaxNumberLabels());
+                Logger.getInstance().log(assignedInstance);
+
+                allAssignedInstance.add(assignedInstance);
+            }
+
+        }
+        OutputWriter.write(allAssignedInstance,dataSet,allUsers);
+
+    }
+}
+
