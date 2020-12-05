7bec4f988c3036e06d53ca5536c0ab79a799bf46
Efe Berke Erkeskin <efeberke1999@hotmail.com>
2020-12-05 18:28:10 +0300

    Implemented Parser classes in general. Parser classes converted with the help of gson library.

---
+++ src/main/java/GRP17/IOController/Parser.java
@@ -0,0 +1,17 @@
+package GRP17.IOController;
+
+ public abstract class Parser {
+
+     /*
+     Custom interface for all the Parsing mechanisms.
+      */
+
+     protected String fileName;
+
+
+     Parser(String fileName){
+         this.fileName = fileName;
+     }
+
+     abstract Object parse();
+}
