21ebb6fbe32903428abd1ac6a065b48330722185
Zeynep Alıcı <aliciizeynep@gmail.com>
2020-12-24 19:48:51 +0300

    Merge branch 'master' of /Users/morsi/CSE3063F20P1_GRP17 with conflicts.

--- src/main/java/GRP17/Main.java
+++ src/main/java/GRP17/Main.java
@@ -2,25 +2,87 @@ package GRP17;
 
 import GRP17.IOController.*;
 import GRP17.Models.*;
+import GRP17.UserModels.User;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
 
         Parser configParser = new ConfigParser("assets/config.json");
+
         OutputWriter outputWriter = new OutputWriter("assets/output.json");
-        CacheManager cacheManager = new CacheManager("assets/cache.json");
-        ReportWriter reportWriter = new ReportWriter( "assets/report.json");
 
 
         ConfigSet configSet = ((ConfigParser) configParser).parse();
 
+        int reportId = configSet.getCurrentDataset().getId();
+        String reportName = "assets/report" + reportId + ".json";
+        ReportWriter reportWriter = new ReportWriter(reportName);
 
-        LabellingMechanism labellingMechanism = new LabellingMechanism(configSet, reportWriter, cacheManager, outputWriter);
 
-        labellingMechanism.startLabeling();
+        List<DataSet> simulationDataSets = new ArrayList<>();
+        List<AssignedInstance> simulationAssignedInstances = new ArrayList<>();
+        List<User> simulationUsers = new ArrayList<>();
+        List<Instance> simulationInstances = new ArrayList<>();
 
+
+        // TODO: ÖNCEKİ SİMULASYONUN OUTPUTUNU OKU. CURRENTLARA ATTRIBUTELARIYLA BİRLİKTE EKLE.
+
+        DataSet dataSet = configSet.getCurrentDataset();
+        dataSet.setId(configSet.getCurrentDatasetId());
+
+        List<User> allUsersAssignedToCurrent = configSet.getUsers();
+
+        List<Instance> allInstancesOfCurrentDataset = dataSet.getInstances();
+        List<Label> allLabelsOfCurrentDataset = dataSet.getLabels();
+
+        dataSet.setInstances(allInstancesOfCurrentDataset);
+        dataSet.setUsers(allUsersAssignedToCurrent);
+        dataSet.setLabels(allLabelsOfCurrentDataset);
+
+        if (!simulationDataSets.contains(dataSet)) {
+            simulationDataSets.add(dataSet);
+        }
+
+
+        for (Instance instance : allInstancesOfCurrentDataset) {
+            for (User user : allUsersAssignedToCurrent) {
+                boolean consistency = (Math.random() < user.getConsistencyCheckProbability());
+                AssignedInstance assignedInstance;
+                if (consistency && user.hasLabelledInstance()) {
+                    assignedInstance = user.relabelAlreadyLabelledInstance(allLabelsOfCurrentDataset, dataSet.getMaxNumberLabels());
+
+                } else {
+                    assignedInstance = user.assignLabel(instance, allLabelsOfCurrentDataset, dataSet.getMaxNumberLabels());
+                }
+
+                simulationAssignedInstances.add(assignedInstance);
+                if (!simulationUsers.contains(user))
+                    simulationUsers.add(user);
+
+                if (!simulationInstances.contains(instance)) {
+                    simulationInstances.add(instance);
+                }
+
+                user.addDatasetID(dataSet.getId());
+                // TODO: CURRENTLARI OUTPUTA ATTRUBUTELARIYLA BIRLIKTE YAZDIR.
+
+                reportWriter.Write(simulationDataSets, simulationUsers, simulationInstances, simulationAssignedInstances);
+                simulation.serialize(simulationDataSets,simulationInstances,simulationUsers,simulationAssignedInstances);
+
+            }
+        }
+
+        outputWriter.write(simulationAssignedInstances, dataSet, allUsersAssignedToCurrent);
 
     }
 }
