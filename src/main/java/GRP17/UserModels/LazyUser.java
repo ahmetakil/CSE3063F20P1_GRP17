4597af0b08b6533dfb2840323a7ff288c01b6bfe
Rıdvan San <sanridvan@gmail.com>
2021-01-02 19:17:34 +0300

    New user type as LazyUser has added

---
+++ src/main/java/GRP17/UserModels/LazyUser.java
@@ -0,0 +1,28 @@
+package GRP17.UserModels;
+
+
+import GRP17.Models.Instance;
+import GRP17.Models.Label;
+
+import java.util.*;
+
+
+public class LazyUser extends User {
+
+    public LazyUser(Integer id, String name, String type, double consistencyCheckProbability) {
+        super(id, name, type, consistencyCheckProbability);
+    }
+
+    public List<Label> pickLabel(List<Label> allLabels, int maxNumberOfLabelsPerInstance, Instance instance) {
+        int maxLength = 0;
+        List<Label> picks = new ArrayList<>();
+        Label shortest =  allLabels.get(0);
+        for(Label label: allLabels){
+            if(label.getName().length() < shortest.getName().length()){
+                shortest = label;
+            }
+        }
+        picks.add(shortest);
+        return picks;
+    }
+}
